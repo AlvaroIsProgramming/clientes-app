2c4da2fd1db3d8266e3d69119eed0dc91bcb9cb5
aghsk <aghski@gmail.com>
2021-04-07 18:30:57 +0200

    Primera carpeta del curso hecha.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a Angular:D';
+
+  curso: string = 'Curso Spring5 con Angular';
+  alumno: string = 'Alvaro';
 }
