03a93b089ddf1a5da344f52a921f6291b23db547
aghsk <aghski@gmail.com>
2021-04-08 12:58:40 +0200

    Todo lo referente a las primeras carpetas con este entorno acabado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/clientes', pathMatch: 'full'},
+  {path: 'directivas', component: DirectivaComponent},
+  {path: 'clientes', component: ClientesComponent},
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +25,10 @@ import { ClientesComponent } from './clientes/clientes.component';
     ClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Pedro', apellido: 'Brown', email: 'pbrown@gmail.com', createAt: '2021-12-11'},
+  {id: 2, nombre: 'Juan', apellido: 'Rodriguez', email: 'jrodriguez@gmail.com', createAt: '2021-12-11'},
+  {id: 3, nombre: 'Sara', apellido: 'Lopez', email: 'slopez@gmail.com', createAt: '2021-12-11'},
+  {id: 4, nombre: 'Alba', apellido: 'Cobos', email: 'acobos@gmail.com', createAt: '2021-12-11'},
+  {id: 5, nombre: 'Francis', apellido: 'Garcia', email: 'fgarcia@gmail.com', createAt: '2021-12-11'},
+  {id: 6, nombre: 'Diego', apellido: 'Fernandez', email: 'dfernandez@gmail.com', createAt: '2021-12-11'},
+  {id: 7, nombre: 'Jade', apellido: 'Morrow', email: 'jmorrow@gmail.com', createAt: '2021-12-11'}
+];
